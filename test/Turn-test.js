a49495ed1db6ad326684218091aea125d0ca9e59
Nikki <piknik@me.com>
2021-02-03 12:24:34 -0700

    reorganize turn tests

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,86 +5,63 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', () => {
+  describe('general functionality', () => {
+    it('should be a function', () => {
+      const turn = new Turn();
 
-  it('should be a function', () => {
-    const turn = new Turn();
+      expect(Turn).to.be.a('function');
+      expect(turn).to.exist;
+    });
 
-    expect(Turn).to.be.a('function');
-    expect(turn).to.exist;
-  });
+    it('should be an instance of Turn', () => {
+      const turn = new Turn();
+      expect(turn).to.be.an.instanceof(Turn);
+    });
+  })
 
-  it('should be an instance of Turn', () => {
-    const turn = new Turn();
-    expect(turn).to.be.an.instanceof(Turn);
-  });
-
-  it('should have a string for user guess and a card instance', () => {
+  describe('specific functionality happy path', () => {
     const card = new Card(2, 'What is a comma-separated list of related values?',
       ['array', 'object', 'function'],
       'array');
     const turn = new Turn('array', card);
 
-    expect(turn.guess).to.equal(card.correctAnswer)
-  });
+    it('should have a string for user guess and a card instance', () => {
+      expect(turn.guess).to.equal(card.correctAnswer)
+    });
 
-  it('should have a method returnGuess that returns the guess', () => {
-    const card = new Card(2, 'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array');
-    const turn = new Turn('array', card);
+    it('should have a method returnGuess that returns the guess', () => {
+      turn.returnGuess();
+      expect(turn.guess).to.equal('array');
+    });
 
-    turn.returnGuess();
+    it('should have a method returnCard that returns the card', () => {
+      turn.returnCard();
+      expect(turn.card).to.equal(card);
+    });
 
-    expect(turn.guess).to.equal('array');
-  });
+    it('should evaluate to true if the guess is correct', () => {
+      expect(turn.evaluateGuess()).to.equal(true);
+    });
 
-  it('should have a method returnCard that returns the card', () => {
-    const card = new Card(2, 'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array');
-    const turn = new Turn('array', card);
+    it('should return positive feedback about correct guess', () => {
+      turn.evaluateGuess();
+      expect(turn.giveFeedback()).to.equal('correct!');
+    })
+  })
 
-    turn.returnCard();
-
-    expect(turn.card).to.equal(card);
-  });
-
-  it('should evaluate to true if the guess is correct', () => {
-    const card = new Card(2, 'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array');
-    const turn = new Turn('array', card);
-
-    expect(turn.evaluateGuess()).to.equal(true);
-  });
-
-  it('should evaluate to false if the guess is not correct', () => {
+  describe('specific functionality sad path', () => {
     const card = new Card(2, 'What is a comma-separated list of related values?',
       ['array', 'object', 'function'],
       'array');
     const turn = new Turn('object', card);
 
-    expect(turn.evaluateGuess()).to.equal(false);
+    it('should evaluate to false if the guess is not correct', () => {
+      expect(turn.evaluateGuess()).to.equal(false);
+    });
+
+    it('should return negative feedback about incorrect guess', () => {
+      turn.evaluateGuess();
+      expect(turn.giveFeedback()).to.equal('incorrect!');
+    });
   });
-
-  it('should return positive feedback about correct guess', () => {
-    const card = new Card(2, 'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array');
-    const turn = new Turn('array', card);
-    turn.evaluateGuess();
-
-    expect(turn.giveFeedback()).to.equal('correct!');
-  })
-
-  it('should return negative feedback about incorrect guess', () => {
-    const card = new Card(2, 'What is a comma-separated list of related values?',
-      ['array', 'object', 'function'],
-      'array');
-    const turn = new Turn('object', card);
-    turn.evaluateGuess();
-
-    expect(turn.giveFeedback()).to.equal('incorrect!');
-  })
-
-})
+});
